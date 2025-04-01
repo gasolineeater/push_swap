a7ab8f5a5f4e99e14236d37be24ce0f818538c7b
ezekaj <elvizekaj02@gmail.com>
2025-04-01 23:30:05 +0200

    Update push_swap.h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,6 @@
 # include "../libft/libft.h"
 # include "../printf/ft_printf.h"
 
-/* Data Structures */
 typedef struct s_node
 {
 	int				value;
@@ -19,16 +18,6 @@ typedef struct s_stack
 	int		size;
 }				t_stack;
 
-/* Stack Functions */
 t_stack	*init_stack(void);
-void	free_stack(t_stack *stack);
-int		push_to_stack(t_stack *stack, int value);
-int		pop_from_stack(t_stack *stack);
-
-/* Parse and Validation */
-int		parse_args(int argc, char **argv, t_stack *stack_a);
-int		ft_atoi_safe(const char *str, int *result);
-int		has_duplicates(t_stack *stack);
-int		is_sorted(t_stack *stack);
 
 # endif
