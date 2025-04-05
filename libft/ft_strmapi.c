de2ee09524d5df538c6d8afc5f4d32abe6bbcc9b
gasolineeater <ezekaj@42.heilbronn.de>
2025-04-05 10:11:31 +0200

    Edit: Checks

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ezekaj <ezekaj@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: gasolineeater <gasolineeater@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 17:18:44 by ezekaj            #+#    #+#             */
-/*   Updated: 2024/11/07 12:09:05 by ezekaj           ###   ########.fr       */
+/*   Updated: 2025/04/05 10:09:35 by gasolineeat      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 	size_t	lens;
 	char	*news;
 
+	if (!s || !f)
+		return (NULL);
 	i = 0;
 	lens = ft_strlen(s);
 	news = ft_calloc(lens + 1, sizeof(char));
